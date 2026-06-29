b52e73cd83050f23683a8de61c82e45146f4484b
Paradeluxe <paradeluxe3726@gmail.com>
2026-06-30 02:08:57 +0800

    chore: bump version to 0.8.0.post3 for TestPyPI release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='praasper',
-version='0.8.0.post2',
+version='0.8.0.post3',
     description='VAD-Enhanced ASR Framework for Researchers',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
