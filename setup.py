a55c541c861f92a886a4a2eea63860b13ef69c5f
Paradeluxe <paradeluxe3726@gmail.com>
2026-06-30 13:17:37 +0800

    chore: bump version to 0.8.0.post4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='praasper',
-version='0.8.0.post3',
+version='0.8.0.post4',
     description='VAD-Enhanced ASR Framework for Researchers',
     long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
